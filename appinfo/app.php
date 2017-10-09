8ec2d14fb22208164f222ae43b001c4802877e2b
root <root@crlt-k15.cdn.aarnet.edu.au>
2017-10-09 03:44:57 +0000

    Improve compatibility with owncloud 8.2 as well as cernbox

--- appinfo/app.php
+++ appinfo/app.php
@@ -18,15 +18,16 @@ require_once __DIR__ . '/autoload.php';
 $app = new App('swanviewer');
 $container = $app->getContainer();
 
-
-$domains = \OC::$server->getConfig()->getSystemValue("cbox.swan.cspdomains", ['cdnjs.cloudflare.com', 'root.cern.ch']);
-$policy = new \OCP\AppFramework\Http\EmptyContentSecurityPolicy();
-foreach($domains as $domain) {
-        $policy->addAllowedScriptDomain($domain);
-        $policy->addAllowedFrameDomain($domain);
-        $policy->addAllowedStyleDomain($domain);
+if (\OCP\Util::getVersion()[0] >= 10) {
+	$domains = \OC::$server->getConfig()->getSystemValue("cbox.swan.cspdomains", ['cdnjs.cloudflare.com', 'root.cern.ch']);
+	$policy = new \OCP\AppFramework\Http\EmptyContentSecurityPolicy();
+	foreach($domains as $domain) {
+	        $policy->addAllowedScriptDomain($domain);
+	        $policy->addAllowedFrameDomain($domain);
+	        $policy->addAllowedStyleDomain($domain);
+	}
+	\OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
 }
-\OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
 
 \OCP\Util::addScript('swanviewer', 'script');
 \OCP\Util::addStyle('swanviewer', 'style');
