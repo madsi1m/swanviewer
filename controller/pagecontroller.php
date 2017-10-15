dbc288b53dd640d026d79502f53cb7d87990a015
Michael D'Silva <michael.dsilva@aarnet.edu.au>
2017-10-15 22:22:31 +0000

    remove old comments

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -168,7 +168,6 @@ class PageController extends Controller {
 		$descriptorspec = array(
 			0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
 			1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-			//2 => array("file", dirname($this->inputHack) . '/error_log.log', "a") // write logs here
 			2 => array("pipe", "w") // error
 		);
 
